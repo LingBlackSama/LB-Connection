828d4c9e033801796a685bc68ec97718ff11979c
Sakura_Sama <88409650+LingBlackSama@users.noreply.github.com>
2022-11-27 18:59:44 -0800

    Update LBConnection.lua

--- LBConnection.lua
+++ LBConnection.lua
@@ -14,13 +14,81 @@ local BindableRecieveRemote: RemoteEvent = script.BindableRecieveRemote
 local InvokeBindableSentRemote: RemoteEvent = script.InvokeBindableSentRemote
 local InvokeBindableRecieveRemote: RemoteEvent = script.InvokeBindableRecieveRemote
 local InvokeSentRemote: RemoteEvent = script.InvokeSentRemote
-local InvokeRecieveRenmote: RemoteEvent = script.InvokeRecieveRenmote
+local InvokeRecieveRemote: RemoteEvent = script.InvokeRecieveRemote
 
 local Server: boolean = RS2:IsServer()
 
 local LBConnection: table = {};
 local CallBackList: table = {};
 local EventListenerList: table = {};
+
+local function OneWay(Remote: RemoteEvent, plr: Player, ID: string|number, ExtraData: table)
+    if Server then
+        Remote:FireClient(plr, ID, unpack(ExtraData))
+    else
+        Remote:FireServer(ID, unpack(ExtraData))
+    end
+end
+
+local function OneWayArray(Remote: RemoteEvent, Array: table, plr: Player, ID: string|number, ExtraData: table)
+    if Server then
+        for _, plr: Player in ipairs(Array) do
+            Remote:FireClient(plr, ID, unpack(ExtraData))
+        end
+    else
+        for _ in ipairs(Array) do
+            Remote:FireServer(ID, unpack(ExtraData))
+        end
+    end
+end
+
+local function Invoke(Remote: RemoteEvent, plr: Player, ID: string|number, TimeOut: IntValue, ExtraData: table)
+    local Thread: thread = coroutine.running()
+    local UUID: GUID = Http:GenerateGUID(false)
+    local Resume: boolean = false
+    ExtraData = {UUID, unpack(ExtraData)}
+
+    EventListenerList[UUID] = function(PlayerWhoFired: Player, CallbackState: boolean, ...: any)
+        if PlayerWhoFired ~= plr then return end
+        Resume = true
+        EventListenerList[UUID] = nil
+        if CallbackState then
+            task.spawn(Thread, true, ...)
+        else
+            task.spawn(Thread, false)
+        end
+    end
+
+    task.delay(TimeOut, function()
+        if Resume then return end
+        EventListenerList[UUID] = nil
+        task.spawn(Thread, false)
+    end)
+
+    OneWay(Remote, plr, ID, ExtraData)
+
+    return coroutine.yield()
+end
+
+local function ClientCallBackFunc(ID: string|number, ...: any)
+    local CallBack: F = CallBackList[ID]
+    if CallBack then
+        CallBack(...)
+    end
+end
+
+local function ServerCallBackFunc(plr: Player, ID: string|number, ...: any)
+    local CallBack: F = CallBackList[ID]
+    if CallBack then
+        CallBack(...)
+    end
+end
+
+local function InvokeRecieve(plr: Player, UUID: string, CallbackState: boolean, ...: any)
+    local EventListener: F = EventListenerList[UUID]
+    if not EventListener then return end
+    EventListener(plr, CallbackState, ...)
+end
 
 local function GetNearPlayer(chr: Charatcer, Radius: IntValue): table
     local ListOfPlayersInRange: table = {};
@@ -42,103 +110,36 @@ end
 
 -- One way connection (Client <-> Server)
 function LBConnection.Fire(plr: Player, ID: string|number, ...: any)
-    if Server then
-        SentRemote:FireClient(plr, ID, ...)
-    else
-        SentRemote:FireServer(ID, ...)
-    end
+    OneWay(SentRemote, plr, ID, {...})
 end
 
--- One way connection to all clients
-function LBConnection.FireAllClient(ID: string|number, ...: any)
+-- Connection between (All Clients -> Server) and (Sever -> All Clients)
+function LBConnection.FireAll(plr: Player, ID: string|number, ...: any)
     local PlayerList: table = Plr:GetPlayers()
-    for _, plr in pairs(PlayerList) do
-        SentRemote:FireClient(plr, ID, ...)
-    end
+    OneWayArray(SentRemote, PlayerList, plr, ID, {...})
 end
 
--- One way connection to all clients in the radius
+-- One way connection in the radius [(Client(s) -> Server) or (Server -> Client(s))]
 function LBConnection.FireDistance(plr: Player, ID: string|number, RenderDistance: IntValue, ...: any)
     local chr: Character = plr.Character or plr.CharacterAdded:Wait()
     RenderDistance = RenderDistance or 20
     local RenderPlayers: table = GetNearPlayer(chr, RenderDistance)
-
-    for _, plr in pairs(RenderPlayers) do
-        SentRemote:FireClient(plr, ID, ...)
-    end
+    OneWayArray(SentRemote, RenderPlayers, plr, ID, {...})
 end
 
 -- Connection between (Client -> Client) and (Server -> Server)
 function LBConnection.FireBindable(plr: Player, ID: string|number, ...: any)
-    if Server then
-        BindableSentRemote:FireClient(plr, ID, ...)
-    else
-        BindableSentRemote:FireServer(ID, ...)
-    end
-end
-
--- Connection between (Client -> Client -> Client) and (Server -> Server -> Server)
-function LBConnection.InvokeBindable(plr: Player, ID: string|number, TimeOut: IntValue, ...: any): any
-    local Thread: thread = coroutine.running()
-    local UUID: GUID = Http:GenerateGUID(false)
-    local Resume: boolean = false
-
-    EventListenerList[UUID] = function(PlayerWhoFired: Player, CallbackState: boolean, ...: any)
-        if PlayerWhoFired ~= plr then return end
-        Resume = true
-        EventListenerList[UUID] = nil
-        if CallbackState then
-            task.spawn(Thread, true, ...)
-        else
-            task.spawn(Thread, false)
-        end
-    end
-
-    task.delay(TimeOut, function()
-        if Resume then return end
-        EventListenerList[UUID] = nil
-        task.spawn(Thread, false)
-    end)
-
-    if Server then
-        InvokeBindableSentRemote:FireClient(plr, ID, UUID, ...)
-    else
-        InvokeBindableSentRemote:FireServer(ID, UUID, ...)
-    end
-
-    return coroutine.yield()
+    OneWay(BindableSentRemote, plr, ID, {...})
 end
 
 -- Conection between (Client -> Server -> Client) and (Server -> Client -> Server)
 function LBConnection.Invoke(plr: Player, ID: string|number, TimeOut: IntValue, ...: any): any
-    local Thread: thread = coroutine.running()
-    local UUID: GUID = Http:GenerateGUID(false)
-    local Resume: boolean = false
+    return Invoke(InvokeSentRemote, plr, ID, TimeOut, {...})
+end
 
-    EventListenerList[UUID] = function(PlayerWhoFired: Player, CallbackState: boolean, ...: any)
-        if PlayerWhoFired ~= plr then return end
-        Resume = true
-        EventListenerList[UUID] = nil
-        if CallbackState then
-            task.spawn(Thread, true, ...)
-        else
-            task.spawn(Thread, false)
-        end
-    end
-
-    task.delay(TimeOut, function()
-        if Resume then return end
-        EventListenerList[UUID] = nil
-        task.spawn(Thread, false)
-    end)
-
-    if Server then
-        InvokeSentRemote:FireClient(plr, ID, UUID, ...)
-    else
-        InvokeSentRemote:FireServer(ID, UUID, ...)
-    end
-
-    return coroutine.yield()
+-- Connection between (Client -> Client -> Client) and (Server -> Server -> Server)
+function LBConnection.InvokeBindable(plr: Player, ID: string|number, TimeOut: IntValue, ...: any): any
+    return Invoke(InvokeBindableSentRemote, plr, ID, TimeOut, {...})
 end
 
 -- Set up the callback
@@ -153,23 +154,13 @@ end
 
 -- < Connections > --
 if Server then
-    SentRemote.OnServerEvent:Connect(function(plr: Player, ID: string|number, ...: any)
-        local CallBack: F = CallBackList[ID]
-        if CallBack then
-            CallBack(...)
-        end
-    end)
+    SentRemote.OnServerEvent:Connect(ServerCallBackFunc)
 
     BindableSentRemote.OnServerEvent:Connect(function(plr: Player, ID: string|number, ...: any)
         BindableRecieveRemote:FireClient(plr, ID, ...)
     end)
 
-    BindableRecieveRemote.OnServerEvent:Connect(function(plr: Player, ID: string|number, ...: any)
-        local CallBack: F = CallBackList[ID]
-        if CallBack then
-            CallBack(...)
-        end
-    end)
+    BindableRecieveRemote.OnServerEvent:Connect(ServerCallBackFunc)
 
     InvokeBindableSentRemote.OnServerEvent:Connect(function(plr: Player, ID: string|number, UUID: string, ...: any)
         InvokeBindableRecieveRemote:FireClient(plr, plr, ID, UUID, ...)
@@ -188,35 +179,21 @@ if Server then
     InvokeSentRemote.OnServerEvent:Connect(function(plr: Player, ID: string|number, UUID: string, ...: any)
         local CallBack: F = CallBackList[ID]
 		if CallBack then
-			InvokeRecieveRenmote:FireClient(plr, plr, UUID, true, CallBack(...))
+			InvokeRecieveRemote:FireClient(plr, plr, UUID, true, CallBack(...))
 		else
-			InvokeRecieveRenmote:FireClient(plr, plr, UUID, false)
+			InvokeRecieveRemote:FireClient(plr, plr, UUID, false)
 		end
     end)
 
-    InvokeRecieveRenmote.OnServerEvent:Connect(function(plr: Player, UUID: string, CallbackState: boolean, ...: any)
-		local EventListener: F = EventListenerList[UUID]
-		if not EventListener then return end
-		EventListener(plr, CallbackState, ...)
-	end)
+    InvokeRecieveRemote.OnServerEvent:Connect(InvokeRecieve)
 else
-    SentRemote.OnClientEvent:Connect(function(ID: string|number, ...: any)
-        local CallBack: F = CallBackList[ID]
-        if CallBack then
-            CallBack(...)
-        end
-    end)
+    SentRemote.OnClientEvent:Connect(ClientCallBackFunc)
 
     BindableSentRemote.OnClientEvent:Connect(function(ID: string|number, ...: any)
         BindableRecieveRemote:FireServer(ID, ...)
     end)
 
-    BindableRecieveRemote.OnClientEvent:Connect(function(ID: string|number, ...: any)
-        local CallBack: F = CallBackList[ID]
-        if CallBack then
-            CallBack(...)
-        end
-    end)
+    BindableRecieveRemote.OnClientEvent:Connect(ClientCallBackFunc)
 
     InvokeBindableSentRemote.OnClientEvent:Connect(function(ID: string|number, UUID: string, ...: any)
         InvokeBindableRecieveRemote:FireServer(ID, UUID, ...)
@@ -235,17 +212,13 @@ else
     InvokeSentRemote.OnClientEvent:Connect(function(ID: string|number, UUID: string, ...: any)
         local CallBack: F = CallBackList[ID]
 		if CallBack then
-			InvokeRecieveRenmote:FireServer(UUID, true, CallBack(...))
+			InvokeRecieveRemote:FireServer(UUID, true, CallBack(...))
 		else
-			InvokeRecieveRenmote:FireServer(UUID, false)
+			InvokeRecieveRemote:FireServer(UUID, false)
 		end
     end)
 
-    InvokeRecieveRenmote.OnClientEvent:Connect(function(plr: Player, UUID: string, CallbackState: boolean, ...: any)
-		local EventListener: F = EventListenerList[UUID]
-		if not EventListener then return end
-		EventListener(plr, CallbackState, ...)
-	end)
+    InvokeRecieveRemote.OnClientEvent:Connect(InvokeRecieve)
 end
 
 return LBConnection
